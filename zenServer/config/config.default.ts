5bd950d8200f1b2e9b37aeb7a6b7df9aaca57664
Zico <21520993@qqcom>
2020-06-01 02:48:51 +0800

    构建zenServer后台
    1. 初始化egg-ts-graphql-typeorm-seneca微服务体系
    2. 增加graphql配置与插件引用

---
+++ zenServer/config/config.default.ts
@@ -0,0 +1,24 @@
+import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
+import graphqlConfig from './config.graphql';
+
+export default (appInfo: EggAppInfo) => {
+  const config = {} as PowerPartial<EggAppConfig>;
+
+  // override config from framework / plugin
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1590863397980_2851';
+  config.graphql = graphqlConfig;
+  // add your egg config in here
+  config.middleware = [ 'graphql' ];
+
+  // add your special config in here
+  const bizConfig = {
+    sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
+  };
+
+  // the return config will combines to EggAppConfig
+  return {
+    ...config,
+    ...bizConfig,
+  };
+};
---
+++ zenServer/config/config.graphql.ts
@@ -0,0 +1,38 @@
+// ts-ignore
+export default () => ({
+  router: '/graphql',
+  // 是否加载到 app 上，默认开启
+  app: true,
+  // 是否加载到 agent 上，默认关闭
+  agent: false,
+  // 是否加载开发者工具 graphiql, 默认开启。路由同 router 字段。使用浏览器打开该可见。
+  graphiql: true,
+  // 是否设置默认的Query和Mutation, 默认关闭
+  defaultEmptySchema: true,
+  // graphQL 路由前的拦截器
+  // * onPreGraphQL(ctx) {
+  // },
+  // 开发工具 graphiQL 路由前的拦截器，建议用于做权限操作(如只提供开发者使用)
+  // * onPreGraphiQL(ctx) {
+  // },
+  // apollo server的透传参数，参考[文档](https://www.apollographql.com/docs/apollo-server/api/apollo-server/#parameters)
+  apolloServerOptions: {
+    rootValue,
+    formatError,
+    formatResponse,
+    mocks,
+    schemaDirectives,
+    introspection,
+    playground,
+    debug,
+    validationRules,
+    tracing,
+    cacheControl,
+    subscriptions,
+    engine,
+    persistedQueries,
+    cors,
+  },
+}
+);
+
