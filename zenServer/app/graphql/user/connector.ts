621f58952ee1bd137035d44c9f0da851541d535f
Zico <21520993@qqcom>
2020-06-03 01:43:13 +0800

    加入了DataLoader独立插件
    1. 还没有解决graphql调用typeOrm的问题

--- zenServer/app/graphql/user/connector.ts
+++ zenServer/app/graphql/user/connector.ts
@@ -1,11 +1,17 @@
+import * as DataLoader from 'dataloader';
+
 class UserConnector {
   ctx: any
-
+  loader: any
   constructor(ctx) {
     this.ctx = ctx;
+    this.loader = new DataLoader(this.getUser.bind(this));
   }
   async getUser(account) {
-    return await this.ctx.service.user.getUser(account);
+    // return await this.ctx.service.user.getUser(account)
+    return this.ctx.repo.User.createQueryBuilder()
+      .where('user.account = :account', { account })
+      .getOne();
   }
 }
 module.exports = UserConnector;
--- zenServer/app/graphql/user/resolver.ts
+++ zenServer/app/graphql/user/resolver.ts
@@ -1,7 +1,6 @@
 module.exports = {
   Query: {
     user(root: any, { account }, ctx) {
-      console.log(account, root);
       return ctx.connector.user.getUser(account);
     },
   },
