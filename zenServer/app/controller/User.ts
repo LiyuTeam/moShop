9ca98260884dcee7c3c50b8eff819fd132cbd8a1
Zico <21520993@qq.com>
2020-06-05 09:18:14 +0800

    完善egg-ts-typeorm引用
    1. 调整extend/eggTsTypeorm中的autoEntities功能,只根据config中的typeorm.client和clients的entities路径进行ts 实体文件扫描,并挂在到ctx.autoEntities中

--- zenServer/app/controller/User.ts
+++ zenServer/app/controller/User.ts
@@ -4,7 +4,7 @@ import { Controller } from 'egg';
 export default class UserController extends Controller {
   public async index() {
     const { ctx } = this;
-    this.logger.info('default eti', this.ctx.entities);
+    this.logger.info('default eti', this.ctx.autoEntities);
     ctx.body = await this.ctx.service.user.getUser('test');
   }
 
--- zenServer/app/extend/lib/eggTsTypeorm/index.ts
+++ zenServer/app/extend/lib/eggTsTypeorm/index.ts
@@ -1,12 +1,8 @@
-import { Application, Context } from 'egg';
-import { autoEntities } from './autoEntities';
+import { Application } from 'egg';
 const extendSymbol = Symbol('eggTsTypeorm');
 
 export default {
   application: (app: Application, props: any) => {
     console.log('graphql log', extendSymbol, props, app);
-  },
-  context: (ctx: Context, props: any) => {
-
   },
 };
