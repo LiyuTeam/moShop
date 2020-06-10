744351979aa1fa69ff79cf8860eed7b8ffd79a14
Zico <21520993@qq.com>
2020-06-10 17:38:32 +0800

    增加schema.mutation
    1. 增加user的insUser mutation方法

--- zenServer/app/graphql/user/userSchemaType.ts
+++ zenServer/app/graphql/user/userSchemaType.ts
@@ -1,20 +1,22 @@
-import { GraphQLBoolean, GraphQLEnumType, GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLBoolean, GraphQLEnumType, GraphQLObjectType, GraphQLString, GraphQLInt } from 'graphql';
 
 const statusField = new GraphQLEnumType({
   name: 'status',
   description: '状态',
+  type: GraphQLInt,
   values: {
-    1: { value: '可用' }, 2: { value: '不可用' },
+    1: { value: 1, description: '可用' }, 2: { value: 2, description: '不可用' },
   },
 });
 
 export default new GraphQLObjectType<GraphQLObjectType>({
   name: 'userAccount',
-  fields: {
-    id: { type: GraphQLString },
-    account: { type: GraphQLString },
-    password: { type: GraphQLString },
-    is_test: { type: GraphQLBoolean },
-    status: { type: statusField },
-  },
+  fields: () => ({
+    id: { type: GraphQLString, description: '用户ID' },
+    account: { type: GraphQLString, description: '用户账户' },
+    password: { type: GraphQLString, description: '用户密码' },
+    is_test: { type: GraphQLBoolean, description: '测试账号' },
+    is_admin: { type: GraphQLBoolean, description: '超级管理员' },
+    status: { type: GraphQLInt, args: { unit: { type: statusField } } },
+  }),
 });
